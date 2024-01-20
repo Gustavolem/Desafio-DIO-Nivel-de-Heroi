c30cf4490aa085f57ca54b7bb72a79f7127bdc2c
Gustavolem <gustavolealmattos@gmail.com>
2024-01-20 11:24:52 -0300

    Desafio-DIO-Nivel-de-Heroi
    Desafio Dio Nivel de Heroi
    Gustavo Leal M

---
+++ index.js
@@ -0,0 +1,14 @@
+let NomeHeroi = "Gustavo"
+let XPHeroi = 5221
+let nivel = ""
+
+if (XPHeroi <= 1000) {nivel = "Ferro"}
+else if (XPHeroi <= 2000) {nivel = "Bronze"}
+else if (XPHeroi <= 5000) {nivel = "Prata"}
+else if (XPHeroi <= 7000) {nivel = "Ouro"}
+else if (XPHeroi <= 8000) {nivel = "Platina"}
+else if (XPHeroi <= 9000) {nivel = "Ascendente"}
+else if (XPHeroi <= 10000) {nivel = "Imortal"}
+else if (XPHeroi >= 10001) {nivel = "Radiante"}
+
+console.log ("O Herói de nome" + NomeHeroi + " está no nível de " + nivel)
